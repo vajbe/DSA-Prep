5f4508971f5643e9cd4119a13c29e3362d6753dd
Vivek Ajbe <vajbe.tce@gmail.com>
2024-05-04 20:36:42 +0530

    insertion

--- 09 BST/BST.py
+++ 09 BST/BST.py
@@ -4,4 +4,28 @@ class BSTNode:
         self.right = None
         self.left = None
 
-newBST = newBST(None)
+def insertNode(rootNode, nodeValue):
+    if rootNode.data is None:
+        rootNode.data = nodeValue
+    elif nodeValue <= rootNode.data:        
+        if rootNode.left is None:            
+            node = BSTNode(nodeValue)
+            rootNode.left = node
+        else:
+            insertNode(rootNode.left, nodeValue)
+    else:
+         if rootNode.right is None:            
+            node = BSTNode(nodeValue)
+            rootNode.right = node
+         else:
+            insertNode(rootNode.right, nodeValue)
+    return "Node has been inserted"
+            
+newBST = BSTNode(None)
+print(insertNode(newBST, 80))
+print(insertNode(newBST, 60))
+print(insertNode(newBST, 70))
+print(insertNode(newBST, 90))
+print(newBST.data)
+print(newBST.left.data)
+print(newBST.right.data)
