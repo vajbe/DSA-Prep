9faa7373aa50583b4fb541de284bfb235d758ea5
Vivek Ajbe <vajbe.tce@gmail.com>
2024-05-05 16:51:09 +0530

    insertion of data

--- 10 AVL Tree/AVL.py
+++ 10 AVL Tree/AVL.py
@@ -2,7 +2,7 @@ import QueueList
 
 
 class AVLNode:
-    def __init__(self, data) -> None:
+    def __init__(self, data: int) -> None:
         self.data = data
         self.left = None
         self.right = None
@@ -60,4 +60,64 @@ def search(rootNode: AVLNode, nodeValue):
         search(rootNode.right, nodeValue)
 
 
-root = AVLNode(10)
+def getHeight(rootNode: AVLNode):
+    if not rootNode:
+        return 0
+    return rootNode.height
+
+
+def rotateRight(rootNode: AVLNode):
+    newRoot: AVLNode = rootNode.left
+    rootNode.left = rootNode.left.right
+    newRoot.right = rootNode
+    rootNode.height = 1 + max(getHeight(rootNode.left), getHeight(rootNode.right))
+    newRoot.height = 1 + max(getHeight(newRoot.left), getHeight(newRoot.right))
+    return newRoot
+
+
+def rotateLeft(rootNode: AVLNode):
+    newRoot: AVLNode = rootNode.right
+    rootNode.right = rootNode.right.left
+    newRoot.left = rootNode
+    rootNode.height = 1 + max(getHeight(rootNode.left), getHeight(rootNode.right))
+    newRoot.height = 1 + max(getHeight(newRoot.left), getHeight(newRoot.right))
+    return newRoot
+
+
+def getBalance(rootNode: AVLNode):
+    if rootNode is None:
+        return 0
+    return getHeight(rootNode.left) - getHeight(rootNode.right)
+
+
+def insertNode(rootNode: AVLNode, nodeValue: int):
+    if rootNode is None:
+        return AVLNode(nodeValue)
+
+    if nodeValue < rootNode.data:
+        rootNode.left = insertNode(root.left, nodeValue)
+    else:
+        rootNode.right = insertNode(rootNode.right, nodeValue)
+
+    rootNode.height = 1 + max(getHeight(rootNode.left), getHeight(rootNode.right))
+
+    balance = getBalance(rootNode)
+
+    if balance > 1 and nodeValue < rootNode.left.data:
+        return rotateRight(rootNode)
+    if balance > 1 and nodeValue > rootNode.left.data:
+        rootNode.left = rotateLeft(rootNode.left)
+        return rotateRight(rootNode)
+    if balance < -1 and nodeValue > rootNode.right.data:
+        return rotateLeft(rootNode)
+    if balance < -1 and nodeValue < rootNode.right.data:
+        rootNode.right = rotateRight(rootNode.right)
+        return rotateLeft(rootNode)
+    return rootNode
+
+
+root = AVLNode(5)
+root = insertNode(root, 10)
+root = insertNode(root, 15)
+root = insertNode(root, 20)
+levelOrder(root)
