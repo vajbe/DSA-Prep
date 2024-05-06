c668b2a7b4cf4fa00aade26ac7df00de20ca7153
Vivek Ajbe <vajbe.tce@gmail.com>
2024-05-06 07:29:02 +0530

    Heap level order traversal

--- 08 Tree/MinHeap.py
+++ 08 Tree/MinHeap.py
@@ -9,3 +9,10 @@ def peek(rootNode: Heap) -> int:
     if not rootNode:
         return
     return rootNode.customList[1]
+
+
+def levelOrderTraversal(root: Heap) -> None:
+    if not root:
+        return
+    for i in range(1, root.heapSize + 1):
+        print(root.customList[i])
