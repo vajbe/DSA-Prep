9eced0790db04c71beafbed79b92607355b3f18a
Vivek Ajbe <vajbe.tce@gmail.com>
2024-05-07 07:43:49 +0530

    initial commit for node insertion

--- 08 Tree/MinHeap.py
+++ 08 Tree/MinHeap.py
@@ -16,3 +16,22 @@ def levelOrderTraversal(root: Heap) -> None:
         return
     for i in range(1, root.heapSize + 1):
         print(root.customList[i])
+
+
+def heapifyInsert(rootNode: Heap, index: int, heapType: str) -> None:
+    parentIndex = int(index / 2)
+    if index <= 1:
+        return
+
+    if heapType == 'min':
+        if rootNode.customList[parentIndex] < rootNode.customList[index]:
+            rootNode.customList[parentIndex], rootNode.customList[index] = rootNode.customList[index], rootNode.customList[parentIndex]
+        heapifyInsert(rootNode, parentIndex, heapType)
+    elif heapType == 'max':
+        if rootNode.customList[parentIndex] > rootNode.customList[index]:
+            rootNode.customList[parentIndex], rootNode.customList[index] = rootNode.customList[index], rootNode.customList[parentIndex]
+        heapifyInsert(rootNode, parentIndex, heapType)
+
+def insertNode(rootNode:Heap,heapType:str):
+    if rootNode.heapSize + 1 == rootNode.maxSize:
+        return "Tree is full"
